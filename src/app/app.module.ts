8ef26d797e94cc288fe3b50238fb363ffff338b1
Juserna28 <seserna1028@gmail.com>
2024-04-03 23:37:28 -0500

    Ajuster gráficos
    Fin de la primera parte de los ajustes gráficos de la pokedex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FotoPokemonComponent } from './components/foto-pokemon/foto-pokemon.component';
+import { TarjetaPokemonComponent } from './components/tarjeta-pokemon/tarjeta-pokemon.component';
+import { DetallePokemonComponent } from './components/detalle-pokemon/detalle-pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FotoPokemonComponent,
+    TarjetaPokemonComponent,
+    DetallePokemonComponent
   ],
   imports: [
     BrowserModule,
